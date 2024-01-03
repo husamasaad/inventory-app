c56cfc2ed2c425d14466d8d12e80a23ba807cd61
Husam Asaad <101347296+husamasaad@users.noreply.github.com>
2024-01-03 16:21:45 +0200

    updated allowed origins

--- inventory-backend/config/allowedOrigins.js
+++ inventory-backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@
 const allowedOrigins = [
   'https://inventory-backend-khaki.vercel.app',
   'http://localhost:3000',
-  'https://inventory-frontend-mu.vercel.app/'
+  'https://inventory-frontend-mu.vercel.app',  
 ]
 
 module.exports = allowedOrigins
